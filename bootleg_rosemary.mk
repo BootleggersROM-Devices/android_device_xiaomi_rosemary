545fdd5f896219e65f6e0403fadf09d373c4aad3
Beru Krugermeier <windowz414@gnuweeb.org>
2023-11-18 15:20:14 +0100

    rosemary: Initial Bootleggers
    Signed-off-by: Beru Krugermeier <windowz414@gnuweeb.org>

--- bootleg_rosemary.mk
+++ bootleg_rosemary.mk
@@ -11,18 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
-# Inherit some common PixelBuilds stuff.
-$(call inherit-product, vendor/pb/config/common_full_phone.mk)
-
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_USES_AOSP_RECOVERY := true
-TARGET_BOOT_ANIMATION_RES := 1080
+# Inherit some common Bootleggers stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Build GCamGO
 $(call inherit-product-if-exists, vendor/kasumi/gcgop/config.mk)
 
-PRODUCT_NAME := aosp_rosemary
+PRODUCT_NAME := bootleg_rosemary
 PRODUCT_DEVICE := rosemary
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -34,3 +29,20 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="rosemary-user 12 SP1A.210812.016 V14.0.7.0.TKLMIXM release-keys"
 
 BUILD_FINGERPRINT := Redmi/rosemary_global/rosemary:12/SP1A.210812.016/V14.0.7.0.TKLMIXM:user/release-keys
+
+# Flags
+TARGET_BOOTLEG_ARCH := arm64
+WITH_GAPPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Maintainer
+DEVICE_MAINTAINERS := Beru Krugermeier (windowz414)
+#BOOTLEGGERS_BUILD_TYPE := Shishufied
